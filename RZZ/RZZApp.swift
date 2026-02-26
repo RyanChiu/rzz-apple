1d569d1cee58424f9e53665a1dca9353200bc09e
Ryan Chiu <ryan.chiu@hotmail.com>
2026-02-26 17:01:17 +0800

    Initial Commit

---
+++ RZZ/RZZApp.swift
@@ -0,0 +1,17 @@
+//
+//  RZZApp.swift
+//  RZZ
+//
+//  Created by 赵睿 on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RZZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
